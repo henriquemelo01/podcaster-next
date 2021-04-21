0b522d1b2cc3592fa459222cb4320285b853a12f
Henrique Moraes <henriquemsm@outlook.com>
2021-04-21 16:32:25 -0300

    Começando a implementar o Player

--- src/Components/Player/index.tsx
+++ src/Components/Player/index.tsx
@@ -1,3 +1,18 @@
+import styles from "./styles.module.scss";
+
 export function Player() {
-  return <div></div>;
+  return (
+    <div className={styles.playerContainer}>
+      <header>
+        <img src="playing.svg" />
+        <strong className={styles.playing}>Tocando agora</strong>
+      </header>
+
+      <div className={styles.emptyPlayer}>
+        <strong>Selecione um podcast para ouvir</strong>
+      </div>
+
+      <footer></footer>
+    </div>
+  );
 }
